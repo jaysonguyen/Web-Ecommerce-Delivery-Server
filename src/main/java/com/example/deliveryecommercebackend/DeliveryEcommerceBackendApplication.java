ebf9f03dadf758f5e3dae95b6f523735352516e3
ThaoMy3172 <my.nguyen@smartlook.com.vn>
2023-10-25 16:38:45 +0700

    intial commit

---
+++ src/main/java/com/example/deliveryecommercebackend/DeliveryEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryEcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryEcommerceBackendApplication.class, args);
+    }
+
+}
