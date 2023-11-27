9d8bfc8c80e02ea6caf1c9f15dc3f3d53b65a0ec
Jayson Nguyen <buleberry.nguyen@gmail.com>
2023-11-27 19:00:21 +0700

    receive

--- src/main/java/com/example/deliveryecommercebackend/controller/HistoryDeliveryController.java
+++ src/main/java/com/example/deliveryecommercebackend/controller/HistoryDeliveryController.java
@@ -3,6 +3,7 @@ package com.example.deliveryecommercebackend.controller;
 import com.example.deliveryecommercebackend.DTO.HistoryDeliveryDTO;
 import com.example.deliveryecommercebackend.services.HistoryDeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,20 +16,39 @@ public class HistoryDeliveryController {
     private HistoryDeliveryService hisSer;
 
 
-   @PostMapping
-    public ResponseEntity<?> insertHistoryDelivery(HistoryDeliveryDTO hisDTO) {
-       try {
-           var insertDeliveryHistory = hisSer.confirmReceivePackage(hisDTO);
-           if(insertDeliveryHistory == true) {
-              return ResponseEntity.ok().body("Insert success");
-           }
+//   @PostMapping
+//    public ResponseEntity<?> insertHistoryDelivery(HistoryDeliveryDTO hisDTO) {
+//       try {
+//           var insertDeliveryHistory = hisSer.confirmReceivePackage(hisDTO);
+//           if(insertDeliveryHistory == true) {
+//              return ResponseEntity.ok().body("Insert success");
+//           }
+//
+//           return ResponseEntity.badRequest().body("Insert failed");
+//       }
+//        catch (Exception ex) {
+//           return ResponseEntity.badRequest().body("Error from controller, " + ex.getMessage());
+//        }
+//   }
 
-           return ResponseEntity.badRequest().body("Insert failed");
-       }
-        catch (Exception ex) {
-           return ResponseEntity.badRequest().body("Error from controller, " + ex.getMessage());
+    @PostMapping
+    public ResponseEntity<String> createDeliveryHistory(@RequestParam("order_id") String orderId,
+                                                        @RequestParam("branch_id") String branchId,
+                                                        @RequestParam("shipper_code") String shipperCode,
+                                                        @RequestParam("money_collect") long moneyCollect) {
+        try {
+
+
+            var insertHistory = hisSer.confirmReceivePackage(orderId, branchId, moneyCollect, shipperCode);
+            if(insertHistory == true) {
+                return ResponseEntity.ok().body("Insert success");
+
+            }
+            return new ResponseEntity<>("Delivery history created successfully.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error creating delivery history: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-   }
+    }
 
 
 }
